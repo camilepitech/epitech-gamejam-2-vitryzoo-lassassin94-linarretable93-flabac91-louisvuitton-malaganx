02e8abf232a945eff87f4ac53f461fcd1c343e50
Ryan <ryan.hercule@epitech.eu>
2024-04-05 14:31:14 -0400

    feat: anime sprite hoppy

--- src/main.py
+++ src/main.py
@@ -15,35 +15,48 @@ def close_window():
             return False
     return True
 
-def move_player(player_pos, dt):
+def anime_player(player_sprite_rect, top):
+    if player_sprite_rect.top == top:
+        player_sprite_rect.left += 48;
+        if player_sprite_rect.left == 144:
+            player_sprite_rect.left = 0;
+    else:
+        player_sprite_rect.top = top
+
+def move_player(player_rect, dt, player_sprite_rect):
     keys = pygame.key.get_pressed()
     if keys[pygame.K_z]:
-        if (player_pos.y) >= 0:
-            player_pos.y -= 300 * dt
+        if (player_rect.top) >= 0:
+            player_rect.y -= 300 * dt
+            anime_player(player_sprite_rect, 148)
     if keys[pygame.K_s]:
-        if (player_pos.y + 40) <= 720:
-            player_pos.y += 300 * dt
+        if (player_rect.bottom) <= 720:
+            player_rect.y += 300 * dt
+            anime_player(player_sprite_rect, 0)
     if keys[pygame.K_q]:
-        if (player_pos.x) >= 0:
-            player_pos.x -= 300 * dt
+        if (player_rect.left) >= 0:
+            player_rect.x -= 300 * dt
+            anime_player(player_sprite_rect, 50)
     if keys[pygame.K_d]:
-        if (player_pos.x + 40) <= 1280:
-            player_pos.x += 300 * dt
+        if (player_rect.right) <= 1280:
+            player_rect.x += 300 * dt
+            anime_player(player_sprite_rect, 100)
 
 def run(screen):
     clock = pygame.time.Clock()
     running = True
     dt = 0
-
-    player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+    player_rect = pygame.Rect(screen.get_width() / 2, screen.get_height() / 2, 40, 40)
+    player_image = pygame.image.load("ressources/hoppy.png")
+    player_sprite_rect = pygame.Rect(0, 50, 48, 50)
+    player_sprite_image = player_image.subsurface(player_sprite_rect)
     while running:
         running = close_window()
         screen.fill("white")
-        pygame.draw.rect(screen, "red", pygame.Rect(player_pos.x, player_pos.y , 40, 40))
-
-        move_player(player_pos, dt)
+        move_player(player_rect, dt, player_sprite_rect)
+        player_sprite_image = player_image.subsurface(player_sprite_rect)
+        screen.blit(player_sprite_image, player_rect)
         pygame.display.flip()
-
         dt = clock.tick(60) / 1000
 
 def main():
